3591890893eed96181c58b208ef6290d416e2107
ELBEQQAL94 <elbeqqal.youssef@gmail.com>
2021-05-26 19:41:55 +0100

    settup application

---
+++ components/Content.js
@@ -0,0 +1,11 @@
+import styles from "../styles/content.module.css";
+
+const Content = () => {
+    return (
+        <section className={styles.contentContainer}>
+            <h1>المنتجات الأكثر مبيعًا</h1>
+        </section>
+    );
+};
+
+export default Content;
---
+++ components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <div>
+            Nav bar
+        </div>
+    );
+};
+
+export default NavBar;
